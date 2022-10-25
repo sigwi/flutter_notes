4bc28123350741007cbca6f82670de0c6af3c0a6
sigwi <sigit_wiend@yahoo.com>
2022-10-26 05:20:45 +0700

    revisi mangga

--- mangga/components/input_loc.dart
+++ mangga/components/input_loc.dart
@@ -1,48 +1,20 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:google_place/google_place.dart';
 
-class InputLoc extends StatefulWidget {
-  InputLoc({required this.controller, required this.text});
+class InputLoc extends StatelessWidget {
+  InputLoc({
+    super.key,
+    required this.controller,
+    required this.text,
+    required this.focusNode,
+    required this.suffixIcon,
+    required this.onChanged,
+  });
 
   late TextEditingController controller = TextEditingController();
   late String text;
-
-  @override
-  State<InputLoc> createState() => _InputLocState();
-}
-
-class _InputLocState extends State<InputLoc> {
-  Timer? _debounce;
-
-  DetailsResult? position;
-
-  FocusNode focusNode = FocusNode();
-
-  GooglePlace googlePlace =
-      GooglePlace('AIzaSyCpuxxqFQrsMohkLMa30tJAsc_59sQP-dg');
-
-  //List tempat yg cocok
-  List<AutocompletePrediction> predictions = [];
-
-  autoCompleteSearh(String value) async {
-    var result = await googlePlace.autocomplete.get(value);
-
-    //predictions = list tempat yg cocok dari api, state mounted
-    if (result != null && result.predictions != null && mounted) {
-      print(result.predictions!.first.description);
-      setState(() {
-        predictions = result.predictions!;
-      });
-    }
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    focusNode.dispose();
-  }
+  late FocusNode focusNode;
+  Widget? suffixIcon;
+  Function(String)? onChanged;
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +24,7 @@ class _InputLocState extends State<InputLoc> {
         children: [
           TextField(
             focusNode: focusNode,
-            controller: widget.controller,
+            controller: controller,
             decoration: InputDecoration(
               enabledBorder: OutlineInputBorder(
                 borderSide: BorderSide(color: Colors.white),
@@ -62,64 +34,14 @@ class _InputLocState extends State<InputLoc> {
                 borderSide: BorderSide(color: Colors.deepPurple),
                 borderRadius: BorderRadius.circular(10),
               ),
-              hintText: widget.text,
+              hintText: text,
               hintStyle: TextStyle(color: Colors.grey),
               fillColor: Colors.white,
               filled: true,
               //menampilkan tanda silang di ujung kanan
-              suffixIcon: widget.controller.text.isNotEmpty
-                  ? IconButton(
-                      onPressed: () {
-                        setState(() {
-                          predictions = [];
-                          widget.controller.clear(); //menghilangkan text di x
-                        });
-                      },
-                      icon: Icon(Icons.clear_outlined),
-                    )
-                  : null,
+              suffixIcon: suffixIcon,
             ),
-            onChanged: (value) {
-              //jika debaounce aktif & mau ketik lagi, harus cancel
-              if (_debounce?.isActive ?? false) _debounce!.cancel();
-              _debounce = Timer(const Duration(milliseconds: 1000), () {
-                if (value.isNotEmpty) {
-                  autoCompleteSearh(value);
-                } else {
-                  predictions = [];
-                  position = null;
-                }
-              });
-            },
-          ),
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: predictions.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(
-                  predictions[index].description.toString(),
-                ),
-                //memilih tempat
-                onTap: () async {
-                  final placeID = predictions[index].placeId!;
-                  //detail dari placeID
-                  final details = await googlePlace.details.get(placeID);
-                  if (details != null && details.result != null && mounted) {
-                    //detect which textfield is selected
-                    if (focusNode.hasFocus) {
-                      setState(() {
-                        focusNode = details.result as FocusNode;
-                        //only use the name without detail
-                        widget.controller.text = details.result!.name!;
-                        //utk menghilangkan list d bawah input setelah di click
-                        predictions = [];
-                      });
-                    }
-                  }
-                },
-              );
-            },
+            onChanged: onChanged,
           ),
         ],
       ),
--- mangga/screens/search_screen.dart
+++ mangga/screens/search_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_place/google_place.dart';
 import 'package:mangga/components/input_loc.dart';
 import 'package:provider/provider.dart';
 
@@ -15,6 +18,40 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   final TextEditingController _pickLoc = TextEditingController();
   final TextEditingController _dropLoc = TextEditingController();
+  //-------------------------------------------------------------
+  Timer? _debounce;
+
+  DetailsResult? startPosition;
+  DetailsResult? endPosition;
+
+  late FocusNode startFocusNode;
+  late FocusNode endFocusNode;
+
+  GooglePlace googlePlace =
+      GooglePlace('AIzaSyCpuxxqFQrsMohkLMa30tJAsc_59sQP-dg');
+
+  //List tempat yg cocok
+  List<AutocompletePrediction> predictions = [];
+
+  autoCompleteSearh(String value) async {
+    var result = await googlePlace.autocomplete.get(value);
+
+    //predictions = list tempat yg cocok dari api, state mounted
+    if (result != null && result.predictions != null && mounted) {
+      print(result.predictions!.first.description);
+      setState(() {
+        predictions = result.predictions!;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    startFocusNode.dispose();
+    endFocusNode.dispose();
+  }
+  //-------------------------------------------------------------
 
   @override
   Widget build(BuildContext context) {
@@ -28,44 +65,122 @@ class _SearchScreenState extends State<SearchScreen> {
         leading: BackButton(color: Colors.black),
         title: const Text('Manage Location'),
       ),
-      body: Stack(
+      body: Column(
         children: [
-          Positioned(
-            left: 0,
-            right: 0,
-            top: 0,
-            child: Container(
-              height: 135,
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
-                ),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 5,
-                    spreadRadius: 0.5,
-                    offset: Offset(0.5, 0.5),
-                  )
-                ],
+          Container(
+            height: 135,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
               ),
-              child: Column(
-                children: [
-                  const SizedBox(height: 10),
-                  InputLoc(
-                    controller: _pickLoc,
-                    text: 'Pick Up Location',
-                  ),
-                  const SizedBox(height: 10),
-                  InputLoc(
-                    controller: _dropLoc,
-                    text: 'Drop Location',
-                  ),
-                ],
-              ),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 5,
+                  spreadRadius: 0.5,
+                  offset: Offset(0.5, 0.5),
+                )
+              ],
             ),
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                InputLoc(
+                  focusNode: startFocusNode,
+                  controller: _pickLoc,
+                  text: 'Pick Up Location',
+                  suffixIcon: _pickLoc.text.isNotEmpty
+                      ? IconButton(
+                          onPressed: () {
+                            setState(() {
+                              predictions = [];
+                              _pickLoc.clear(); //menghilangkan text di x
+                            });
+                          },
+                          icon: Icon(Icons.clear_outlined),
+                        )
+                      : null,
+                  onChanged: (value) {
+                    //jika debaounce aktif & mau ketik lagi, harus cancel
+                    if (_debounce?.isActive ?? false) _debounce!.cancel();
+                    _debounce = Timer(const Duration(milliseconds: 1000), () {
+                      if (value.isNotEmpty) {
+                        autoCompleteSearh(value);
+                      } else {
+                        predictions = [];
+                        startPosition = null;
+                      }
+                    });
+                  },
+                ),
+                const SizedBox(height: 10),
+                InputLoc(
+                  focusNode: endFocusNode,
+                  controller: _dropLoc,
+                  text: 'Drop Location',
+                  suffixIcon: _dropLoc.text.isNotEmpty
+                      ? IconButton(
+                          onPressed: () {
+                            setState(() {
+                              predictions = [];
+                              _dropLoc.clear(); //menghilangkan text di x
+                            });
+                          },
+                          icon: Icon(Icons.clear_outlined),
+                        )
+                      : null,
+                  onChanged: (value) {
+                    //jika debaounce aktif & mau ketik lagi, harus cancel
+                    if (_debounce?.isActive ?? false) _debounce!.cancel();
+                    _debounce = Timer(const Duration(milliseconds: 1000), () {
+                      if (value.isNotEmpty) {
+                        autoCompleteSearh(value);
+                      } else {
+                        predictions = [];
+                        endPosition = null;
+                      }
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: predictions.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(
+                  predictions[index].description.toString(),
+                ),
+                //memilih tempat
+                onTap: () async {
+                  final placeID = predictions[index].placeId!;
+                  //detail dari placeID
+                  final details = await googlePlace.details.get(placeID);
+                  if (details != null && details.result != null && mounted) {
+                    //detect which textfield is selected
+                    if (startFocusNode.hasFocus) {
+                      setState(() {
+                        startPosition = details.result;
+                        //only use the name without detail
+                        _pickLoc.text = details.result!.name!;
+                        //utk menghilangkan list d bawah input setelah di click
+                        predictions = [];
+                      });
+                    } else {
+                      setState(() {
+                        endPosition = details.result;
+                        _dropLoc.text = details.result!.name!;
+                        predictions = [];
+                      });
+                    }
+                  }
+                },
+              );
+            },
           ),
         ],
       ),
